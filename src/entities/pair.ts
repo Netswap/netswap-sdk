0116684f6f4f38007bbf2bfd9e69a1576808ed45
0x1688 <Jumorzhu@protonmail.com>
2021-12-06 21:33:48 +0800

    update lp token name

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,7 +55,7 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'NETLP',
+      'NLP',
       'Netswap LP Token'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
